849fe1cf3dd947d29e174cddb55f2bfefd1dac66
root <root@DESKTOP-T4G3ECS.localdomain>
2025-12-06 04:14:56 -0300

    feat: adding backend

--- aws/terraform/providers.tf
+++ aws/terraform/providers.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "tf-challenge-backend"
+    key            = "infrastructure/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
