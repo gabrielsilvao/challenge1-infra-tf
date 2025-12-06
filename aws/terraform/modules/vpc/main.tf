7fdbeb471a135abf9117494c81f41dd7892c645d
root <root@DESKTOP-T4G3ECS.localdomain>
2025-12-06 03:17:51 -0300

    feat: creating vpc using module

---
+++ aws/terraform/modules/vpc/main.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available.names
+  public_subnets = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
+
+  enable_nat_gateway = true
+  single_nat_gateway = var.single_nat_gateway
+  enable_vpn_gateway = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = var.tags
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
